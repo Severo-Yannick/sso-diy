18d4b2de1c03b4ce669d88c1ccbcd77d0ece1b42
Severo-Yannick <severoyannick@gmail.com>
2022-05-30 15:56:11 +0200

    refresh auto

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,9 +1,10 @@
 import express from 'express'
 
 const app = express()
+const PORT = 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  res.send('Bonjour monde !!')
 })
 
-app.listen(3000)
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
