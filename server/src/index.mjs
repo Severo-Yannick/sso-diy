96019048d1a91d1eb5ce57c46a677b7fb4c50a38
Severo-Yannick <severoyannick@gmail.com>
2022-05-31 10:18:53 +0200

    custom css styling

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -3,9 +3,15 @@ import express from 'express'
 
 const app = express()
 const PORT = 3000
+const __dirname = path.resolve();
 
 app.set('view engine', 'pug')
 app.set('views', './src/views')
+
+app.use(
+  '/public/stylesheets',
+  express.static(__dirname + '/public/stylesheets')
+);
 
 app.get('/', (req, res) => {
   res.render('login')
